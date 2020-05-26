81ebf068f6fe8632f439b22259bd1a2e8ce16889
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-26 12:37:40 +0200

    remove System.import()

--- src/phovea.ts
+++ src/phovea.ts
@@ -4,7 +4,7 @@ import {EP_PHOVEA_CORE_LOCALE, ILocaleEPDesc} from 'phovea_core/src/extensions';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   registry.push('importer_value_type', 'categorical', function () {
-    return System.import('./valuetypes');
+    return import('./valuetypes');
   }, {
     'factory': 'categorical',
     'name': 'Categorical',
@@ -12,7 +12,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'real', function () {
-    return System.import('./valuetypes');
+    return import('./valuetypes');
   }, {
     'factory': 'numerical',
     'name': 'Float',
@@ -20,7 +20,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'int', function () {
-    return System.import('./valuetypes');
+    return import('./valuetypes');
   }, {
     'factory': 'numerical',
     'name': 'Integer',
@@ -28,7 +28,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'string', function () {
-    return System.import('./valuetypes');
+    return import('./valuetypes');
   }, {
     'factory': 'string_',
     'name': 'String',
@@ -36,7 +36,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'idType', function () {
-    return System.import('./valuetype_idtype');
+    return import('./valuetype_idtype');
   }, {
     'factory': 'idType',
     'name': 'IDType',
@@ -45,7 +45,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push(EP_PHOVEA_CORE_LOCALE, 'phoveaImporterLocaleEN', function () {
-    return System.import('./assets/locales/en/phovea.json').then(asResource);
+    return import('./assets/locales/en/phovea.json').then(asResource);
   }, <ILocaleEPDesc>{
     ns: 'phovea',
   });
