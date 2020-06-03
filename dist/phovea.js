61257af74ae4d4928f8eb90415b2ceadc020a829
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-06-03 14:00:09 +0200

    fix dynamic imports in phovea.ts

--- dist/phovea.js
+++ dist/phovea.js
@@ -3,35 +3,35 @@ import { EP_PHOVEA_CORE_LOCALE } from 'phovea_core';
 export default function (registry) {
     //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
     registry.push('importer_value_type', 'categorical', function () {
-        return import('./valuetype/valuetypes');
+        return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
     }, {
         'factory': 'categorical',
         'name': 'Categorical',
         'priority': 30
     });
     registry.push('importer_value_type', 'real', function () {
-        return import('./valuetype/valuetypes');
+        return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
     }, {
         'factory': 'numerical',
         'name': 'Float',
         'priority': 10
     });
     registry.push('importer_value_type', 'int', function () {
-        return import('./valuetype/valuetypes');
+        return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
     }, {
         'factory': 'numerical',
         'name': 'Integer',
         'priority': 20
     });
     registry.push('importer_value_type', 'string', function () {
-        return import('./valuetype/valuetypes');
+        return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
     }, {
         'factory': 'string_',
         'name': 'String',
         'priority': 100
     });
     registry.push('importer_value_type', 'idType', function () {
-        return import('./valuetype/valuetype_idtype');
+        return import('./valuetype/valuetype_idtype').then((v) => v.IDTypeUtils);
     }, {
         'factory': 'idType',
         'name': 'IDType',
--- src/phovea.ts
+++ src/phovea.ts
@@ -4,7 +4,7 @@ import {EP_PHOVEA_CORE_LOCALE, ILocaleEPDesc} from 'phovea_core';
 export default function (registry: IRegistry) {
   //registry.push('extension-type', 'extension-id', function() { return import('./extension_impl'); }, {});
   registry.push('importer_value_type', 'categorical', function () {
-    return import('./valuetype/valuetypes');
+    return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
   }, {
     'factory': 'categorical',
     'name': 'Categorical',
@@ -12,7 +12,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'real', function () {
-    return import('./valuetype/valuetypes');
+    return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
   }, {
     'factory': 'numerical',
     'name': 'Float',
@@ -20,7 +20,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'int', function () {
-    return import('./valuetype/valuetypes');
+    return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
   }, {
     'factory': 'numerical',
     'name': 'Integer',
@@ -28,7 +28,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'string', function () {
-    return import('./valuetype/valuetypes');
+    return import('./valuetype/valuetypes').then((v) => v.ValueTypeEditor);
   }, {
     'factory': 'string_',
     'name': 'String',
@@ -36,7 +36,7 @@ export default function (registry: IRegistry) {
   });
 
   registry.push('importer_value_type', 'idType', function () {
-    return import('./valuetype/valuetype_idtype');
+    return import('./valuetype/valuetype_idtype').then((v) => v.IDTypeUtils);
   }, {
     'factory': 'idType',
     'name': 'IDType',
